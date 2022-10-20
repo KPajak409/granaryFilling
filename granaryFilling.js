9a5430a1533673649ca67a06fe8c0f00f2d9fce3
Gloomy-stack <karl.yotl@gmail.com>
2022-10-20 17:49:27 +0200

    Sort optimization

--- granaryFilling.js
+++ granaryFilling.js
@@ -3,8 +3,9 @@ var headerRow = document.querySelector("#production_table thead tr");
 let trCells = headerRow.querySelectorAll("th");
 let th4 = trCells[4];
 let th = document.createElement("th");
-
+var sortOrder = 0;
 th.innerHTML = "<a>Zapelnienie</a>";
+th.style.cursor = "pointer";
 th.addEventListener("click", function () {
   sortTableByGranaryFill(tableRows);
 });
@@ -20,6 +21,11 @@ for (let i = 0; i < tableRows.length; i++) {
   ).toFixed();
 
   let tdCapacity = tableRows[i].insertCell(4);
+  tdCapacity.style.fontWeight = "bold";
+  if (granaryPercentage >= 85) tdCapacity.style.color = "red";
+  if (granaryPercentage < 85 && granaryPercentage > 60)
+    tdCapacity.style.color = "rgb(247 103 0)";
+  if (granaryPercentage <= 60) tdCapacity.style.color = "green";
   tdCapacity.innerText = granaryPercentage + "%";
 }
 
@@ -33,15 +39,45 @@ function getMaxResource(resources) {
 }
 
 function sortTableByGranaryFill(table) {
+  let indexes = [];
+  let tableCells = [];
+  let rawHTML = [];
   for (let i = 0; i < table.length; i++) {
-    for (let j = 0; j < table.length - 1; j++) {
-      let cell1 = table[j].querySelectorAll("td");
-      let cell2 = table[j + 1].querySelectorAll("td");
-      if (parseInt(cell1[4].innerText) > parseInt(cell2[4].innerText)) {
-        let temp = table[j + 1].innerHTML;
-        table[j + 1].innerHTML = table[j].innerHTML;
-        table[j].innerHTML = temp;
+    indexes.push(i);
+    tableCells.push(table[i].querySelectorAll("td")[4].innerText);
+    rawHTML.push(table[i].innerHTML);
+  }
+  if (sortOrder == 0) {
+    for (let i = 0; i < table.length; i++) {
+      for (let j = 0; j < table.length - 1; j++) {
+        if (
+          parseInt(tableCells[indexes[j]]) <
+          parseInt(tableCells[indexes[j + 1]])
+        ) {
+          let temp = indexes[j];
+          indexes[j] = indexes[j + 1];
+          indexes[j + 1] = temp;
+        }
       }
     }
   }
+  if (sortOrder == 1) {
+    for (let i = 0; i < table.length; i++) {
+      for (let j = 0; j < table.length - 1; j++) {
+        if (
+          parseInt(tableCells[indexes[j]]) >
+          parseInt(tableCells[indexes[j + 1]])
+        ) {
+          let temp = indexes[j];
+          indexes[j] = indexes[j + 1];
+          indexes[j + 1] = temp;
+        }
+      }
+    }
+  }
+  for (let i = 0; i < indexes.length; i++)
+    table[i].innerHTML = rawHTML[indexes[i]];
+
+  if (sortOrder) sortOrder = 0;
+  else sortOrder = 1;
 }
